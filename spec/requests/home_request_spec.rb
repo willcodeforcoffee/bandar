965405c041e5b6c2a55825766d8d8d3dd6ede613
Eric Hoff <eric@hoffsoftcorp.com>
2020-11-11 07:18:16 -0700

    Started some RuboCop cleanup

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Homes", type: :request do
+RSpec.describe "Homes", :type => :request do
 
   describe "GET /index" do
     it "returns http success" do
