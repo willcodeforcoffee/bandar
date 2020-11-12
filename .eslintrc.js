0cc6a1d70de80f882073035179701cdc5d3a5698
Eric Hoff <eric@hoffsoftcorp.com>
2020-11-12 13:41:18 -0700

    Setup ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     commonjs: true,
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'airbnb',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -22,4 +26,9 @@ module.exports = {
   rules: {
     "no-console": "warn"
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
