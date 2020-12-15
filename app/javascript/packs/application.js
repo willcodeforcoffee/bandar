e4bff039b2e88bdfba9b679e9ed3ead765be84a6
Eric Hoff <eric@hoffsoftcorp.com>
2020-12-14 18:54:38 -0700

    Turned off Turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start();
-require("turbolinks").start();
+// require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
