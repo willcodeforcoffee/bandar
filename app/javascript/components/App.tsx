e832247eb95200c284c1ca04e6031c1b0aeba18b
Eric Hoff <eric@hoffsoftcorp.com>
2020-12-29 14:46:38 -0700

    Rearranged app to use react_rails and removed unused packs

---
+++ app/javascript/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div className="prose">
+      <h1>React is Working</h1>
+      <p>This means React Rails is working</p>
+    </div>
+  );
+}
+
+export { App };
