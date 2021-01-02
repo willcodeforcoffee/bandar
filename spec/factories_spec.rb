f75bbda7c1f53e25b01cdaca74e413f0dd928823
Eric Hoff <eric@hoffsoftcorp.com>
2021-01-01 18:52:17 -0700

    Fixed a few Rubocop violations

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -3,11 +3,13 @@ require "rails_helper"
 RSpec.describe "FactoryBots" do
   FactoryBot.factories.map(&:name).each do |factory_name|
     describe "#{factory_name} factory" do
-      it "will create a valid object" do
+      # rubocop:disable RSpec/MultipleExpectations
+      it "creates a valid object" do
         o = build(factory_name)
         expect(o.errors.full_messages).to eq([])
         expect(o).to be_valid
       end
+      # rubocop:enable RSpec/MultipleExpectations
     end
   end
 end
