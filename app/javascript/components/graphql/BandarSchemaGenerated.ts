dd3ece13f26dacfba5c40cd5096a8e8bd48eef09
Eric Hoff <eric@hoffsoftcorp.com>
2021-01-24 10:31:04 -0700

    Cleaned up GraphQL logic to work better

--- app/javascript/components/graphql/BandarSchemaGenerated.ts
+++ app/javascript/components/graphql/BandarSchemaGenerated.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
+/* REGENERATE THIS FILE BY RUNNING > bin/rails graphql:create_graphql_schema && yarn run graphql:updateTypes */
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
-import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
@@ -60,6 +60,19 @@ export type Unnamed_1_Query = (
   & Pick<Query, 'testField'>
 );
 
+export type UserInvitationQueryVariables = Exact<{
+  token: Scalars['String'];
+}>;
+
+
+export type UserInvitationQuery = (
+  { __typename?: 'Query' }
+  & { userInvitation?: Maybe<(
+    { __typename?: 'UserInvitation' }
+    & Pick<UserInvitation, 'emailAddress' | 'isAccepted' | 'isSent' | 'isExpired'>
+  )> }
+);
+
 
 export const Document = gql`
     {
@@ -91,17 +104,42 @@ export function useLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<Query, Qu
 export type QueryHookResult = ReturnType<typeof useQuery>;
 export type LazyQueryHookResult = ReturnType<typeof useLazyQuery>;
 export type QueryResult = Apollo.QueryResult<Query, QueryVariables>;
-
-declare module '*/TestQuery.graphql' {
-  import { DocumentNode } from 'graphql';
-  const defaultDocument: DocumentNode;
-  
-
-  export default defaultDocument;
+export const UserInvitationDocument = gql`
+    query userInvitation($token: String!) {
+  userInvitation(token: $token) {
+    emailAddress
+    isAccepted
+    isSent
+    isExpired
+  }
 }
-    
+    `;
 
-
+/**
+ * __useUserInvitationQuery__
+ *
+ * To run a query within a React component, call `useUserInvitationQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserInvitationQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserInvitationQuery({
+ *   variables: {
+ *      token: // value for 'token'
+ *   },
+ * });
+ */
+export function useUserInvitationQuery(baseOptions: Apollo.QueryHookOptions<UserInvitationQuery, UserInvitationQueryVariables>) {
+        return Apollo.useQuery<UserInvitationQuery, UserInvitationQueryVariables>(UserInvitationDocument, baseOptions);
+      }
+export function useUserInvitationLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UserInvitationQuery, UserInvitationQueryVariables>) {
+          return Apollo.useLazyQuery<UserInvitationQuery, UserInvitationQueryVariables>(UserInvitationDocument, baseOptions);
+        }
+export type UserInvitationQueryHookResult = ReturnType<typeof useUserInvitationQuery>;
+export type UserInvitationLazyQueryHookResult = ReturnType<typeof useUserInvitationLazyQuery>;
+export type UserInvitationQueryResult = Apollo.QueryResult<UserInvitationQuery, UserInvitationQueryVariables>;
 
       export interface PossibleTypesResultData {
         possibleTypes: {
