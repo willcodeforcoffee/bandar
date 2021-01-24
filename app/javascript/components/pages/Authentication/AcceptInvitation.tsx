9cd6dc309c867944013de1d01ae6bd11802b38d0
Eric Hoff <eric@hoffsoftcorp.com>
2021-01-24 13:00:16 -0700

    AcceptInvitation form started

--- app/javascript/components/pages/Authentication/AcceptInvitation.tsx
+++ app/javascript/components/pages/Authentication/AcceptInvitation.tsx
@@ -1,11 +1,48 @@
 import * as React from "react";
-import { useQueryString } from "../../utils/useQueryString";
-import { useUserInvitationQuery } from "components/graphql/BandarSchemaGenerated";
+import { useQueryString } from "components/utils/useQueryString";
+import { GraphQLErrorMessage } from "components/graphql/GraphQLErrorMessage";
+import { GraphQLLoadingMessage } from "components/graphql/GraphQLLoadingMessage";
+import { useUserInvitationQuery, UserInvitation } from "components/graphql/BandarSchemaGenerated";
+import { EmailAddressControl, Form, PasswordInputControl, TextInputControl } from "components/forms";
 
 function InvalidInvitation(): JSX.Element {
   return (
     <div>
       <h1>Your invitation is no longer valid. Please contact the administrator for another.</h1>
+    </div>
+  );
+}
+
+interface DisplayUserInvitationProps {
+  userInvitation: UserInvitation;
+}
+
+function DisplayUserInvitation(props: DisplayUserInvitationProps): JSX.Element {
+  console.log("DisplayUserInvitation", props);
+  function theInvitationIsNotValid(userInvitation: UserInvitation) {
+    return userInvitation.isExpired || userInvitation.isAccepted || !userInvitation.isSent;
+  }
+
+  if (theInvitationIsNotValid(props.userInvitation)) {
+    return <InvalidInvitation />;
+  }
+
+  return (
+    <div>
+      <h1>Accept User Invitation</h1>
+      <p>Thank you for accepting the invitation to join this service.</p>
+      <p>Please complete the following information:</p>
+      <Form legend="User Details">
+        <EmailAddressControl
+          key="emailaddress"
+          labelText="Email Address"
+          placeholder="email@example.com"
+          initialValue={props.userInvitation.emailAddress}
+        />
+        <TextInputControl key="fullname" labelText="Full Name" placeholder="Your name, firstname lastname" />
+        <TextInputControl key="username" labelText="User Name" placeholder="Your user name or display name" />
+        <PasswordInputControl key="password" labelText="Password" placeholder="Your password" />
+      </Form>
     </div>
   );
 }
@@ -20,20 +57,10 @@ export function AcceptInvitation(): JSX.Element {
   const { loading, error, data } = useUserInvitationQuery({ variables: { token: token } });
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <GraphQLLoadingMessage />;
   } else if (error) {
-    console.error("An error happened", error);
-    return (
-      <div>
-        <h1>An Error Occurred</h1>
-        <pre>{error}</pre>
-      </div>
-    );
+    return <GraphQLErrorMessage source="AcceptInvitation" error={error} />;
   }
 
-  return (
-    <div id="Authentication_AcceptInvitation">
-      <h1>Accept Invitation Token {token}</h1>
-    </div>
-  );
+  return <DisplayUserInvitation userInvitation={data.userInvitation} />;
 }
