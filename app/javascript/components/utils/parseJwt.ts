f0a47abdc2942454d5d28f3a177a488def2e4fbf
Eric Hoff <eric@hoffsoftcorp.com>
2021-01-28 07:06:03 -0700

    Handle splitting better in Firefox

--- app/javascript/components/utils/parseJwt.ts
+++ app/javascript/components/utils/parseJwt.ts
@@ -1,6 +1,9 @@
 // From: https://stackoverflow.com/a/38552302
 export function parseJwt(token: string): any {
   const base64Url = token.split(".")[1];
+  if (!base64Url) {
+    return null;
+  }
   const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
   const jsonPayload = decodeURIComponent(
     atob(base64)
