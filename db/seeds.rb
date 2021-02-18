4ad2478a90d33d2b847db939f2b8e8465a5f7c06
Eric Hoff <eric@hoffsoftcorp.com>
2021-02-17 20:54:45 -0700

    Created a CSV based database seeding mechanism

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Invoking Database seed"
+
+puts "Checking for [db/seeds/user_credentials.csv]"
+if File.exist?("db/seeds/user_credentials.csv")
+  row_index = 0
+  CSV.foreach("db/seeds/user_credentials.csv", headers: true) do |row|
+    row_index += 1
+    if UserCredential.exists?(:email => row["email_address"])
+      puts "Skipping [#{row_index}:#{row['email_address']}] because that email exists"
+      next
+    end
+
+    user_credential = UserCredential.new({
+                                           email: row["email_address"],
+                                           password: row["password"],
+                                           password_confirmation: row["password"],
+                                         })
+
+    unless user_credential.valid?
+      puts "Skipping [#{row_index}:#{row['email_address']}]: the credentials are invalid"
+      next
+    end
+
+    puts "Seeding UserCredential [#{user_credential.email}]"
+    user_credential.save!
+  end
+else
+  puts "Create the file [db/seeds/user_credentials.csv] to seed users"
+end
+
+puts "Database seed copmlete"
---
+++ db/seeds/README.md
@@ -0,0 +1,15 @@
+# Seeds
+
+This folder contains seed data in the format of CSVs.
+
+Be careful what you put in.
+
+## user_credentials.csv
+
+The file `user_credentials.csv` contains user credentials in the comma-separated (CSV) format:
+
+```
+email_address,password
+```
+
+**Note: The header row as above is required.**
